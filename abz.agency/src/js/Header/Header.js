93a3c82830a375e9fc40d016fa8765365ebc4697
Shakir <azimlishakir@gmail.com>
2019-01-28 21:52:07 -0800

    sEKILLER

--- abz.agency/src/js/Header/Header.js
+++ abz.agency/src/js/Header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../../css/Header/Header.css';
 import {Container,Row,Col} from 'reactstrap';
 import logo from '../../img/logo.svg';
+import icon from '../../icons/sign-out.svg';
+import  circleimg2 from '../../img/user-superstar-2x.jpg';
 class Header extends React.Component{
     render() {
         return (
@@ -9,8 +11,8 @@ class Header extends React.Component{
                 <div className="header_bg">
                     <Container>
                         <Row>
-                            <Col xs="3">
-                                <img src={logo} width="220px" height="50px"/>
+                            <Col xs="2">
+                                <img src={logo} width="135px" height="24px"/>
                             </Col>
 
                             <Col xs="6">
@@ -23,14 +25,14 @@ class Header extends React.Component{
                                 </ul>
                             </Col>
 
-                            <Col xs="3">
-                                <div className="text">
-                                    <p className="name">
-                                        Superstar
-                                    </p>
-                                    <p className="email">
-                                        Superstar@gmail.com
-                                    </p>
+                            <Col xs="4">
+                                <div className="icon_img">
+                                    <div className="text">
+
+                                    </div>
+                                    <img src={circleimg2} className="circleimg2" width="40px" height="40px" alt=""/>
+                                    <img src={icon} className="icon1" alt="" width="24px" height="20px" />
+
                                 </div>
 
                             </Col>
