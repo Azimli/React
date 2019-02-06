7ce480c9c50ac6a79b452245daeec67fe0bc337e
Shakir <azimlishakir@gmail.com>
2019-02-06 03:13:23 -0800

    sd

---
+++ abz.agency/src/js/Container/Container.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import '../../css/Container/Container.css';
+import mob from '../../img/man-mobile.svg';
+import html from '../../img/html.svg';
+import css from '../../img/css.svg';
+import js from  '../../img/javascript.svg';
+import man from '../../img/man-laptop-v1.svg';
+class Container extends React.Component{
+    render() {
+        return(
+            <div>
+              <div className="container">
+                  <div className="center_2560">
+
+                      <div className="con_img">
+                          <img src={mob} alt=""/>
+                      </div>
+
+                      <div className="con_txt">
+                          <h3 className="txt_container">Let's get ac quainted </h3>
+                        <h6 className="con_txt_h6">
+                            I am cool frontend developer
+                        </h6>
+                          <p>
+                              When real users have a slow experience on mobile, they're much less likely to find what they are looking for or <br/>
+                              purchase from you in the future. For many sites this equates to a huge missed opportunity, especially when more <br/>
+                              than half of visits are abandoned if a mobile page takes over 3 seconds to load. <br/>
+                              <br/>
+
+                              Last week, Google Search and Ads teams announced two new speed initiatives to help improve user-experience <br/>
+                              on the web.
+                          </p>
+
+                      </div>
+                      <button className="con_btn">Sign Up</button>
+
+                      {/*  Container 2 ci hissesi  */ }
+
+                      <h3 className="con_2_h3">
+                          About my relationships with <br/>
+                          web-development
+                      </h3>
+
+                      <ul className="lang_prog">
+                          <li>
+                              <img src={html} alt=""/>
+                              <h6>I'm in love with HTML </h6>
+                              <p>
+                                  Hypertext Markup Language (HTML) is the standard <br/>
+                                  markup language for creating web pages and web <br/>
+                                  applications.
+                              </p>
+                          </li>
+
+                          <li>
+                              <img src={css} alt=""/>
+                              <h6>CSS is my best friend</h6>
+                              <p>
+                                  Cascading Style Sheets (CSS) is a  style sheet <br/>
+                                  language used for describing the presentation <br/>
+                                  of a document written in a markup language <br/>
+                                  like HTML.
+                              </p>
+                          </li>
+
+                          <li>
+                              <img src={js} alt=""/>
+                              <h6> JavaScript is my passion </h6>
+                              <p>
+                                  JavaScript is a high-level, interpreted programming <br/>
+                                  language. It is a language which is also characterized <br/>
+                                  as dynamic, weakly typed, prototype-based and <br/>
+                                  multi-paradigm.
+                              </p>
+                          </li>
+                      </ul>
+                  </div>
+
+              </div>
+                <div className="con_bg">
+                    <div className="center_2560">
+                        <div className="con_bg_txt">
+                                <h1>General requirements for the test task</h1>
+                            <p>
+                                Users want to find answers to their questions quickly and data shows that people <br/>
+                                really care about how quickly their pages load. The Search team announced speed <br/>
+                                would be a ranking signal for desktop searches in 2010 and as of this month (July <br/>
+                                2018), page speed will be a ranking factor for mobile searches too. <br/><br/>
+
+                                If you're a developer working on a site, now is a good time to evaluate your <br/>
+                                performance using our speed tools. Think about how performance affects the user <br/>
+                                experience of your pages and consider measuring a variety of real-world <br/>
+                                user-centric performance metrics. <br/> <br/>
+
+                                Are you shipping too much JavaScript? Too many images? Images and JavaScript <br/>
+                                are the most significant contributors to the page weight that affect page load time <br/>
+                                based on data from HTTP Archive and the Chrome User Experience Report - our <br/>
+                                public dataset for key UX metrics as experienced by Chrome users under real-world <br/>
+                                conditions.
+
+
+                            </p>
+                        </div>
+                        
+                        <div className="con_bg_img">
+                            <img src={man} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        );
+    }
+
+}
+
+export default Container;
--- abz.agency/src/js/Header/Header.js
+++ abz.agency/src/js/Header/Header.js
@@ -1,45 +1,70 @@
 import React from 'react';
 import '../../css/Header/Header.css';
-import {Container,Row,Col} from 'reactstrap';
+import '../../css/Header/Header_responsive.css';
 import logo from '../../img/logo.svg';
+import icon from '../../icons/sign-out.svg';
+import  circleimg2 from '../../img/user-superstar-2x.jpg';
 class Header extends React.Component{
     render() {
         return (
             <div>
                 <div className="header_bg">
-                    <Container>
-                        <Row>
-                            <Col xs="3">
-                                <img src={logo} width="220px" height="50px"/>
-                            </Col>
+                    <div className="center_2560">
+                        <div className="logo_header">
+                            <img src={logo} alt=""/> 
+                        </div>
 
-                            <Col xs="6">
-                                <ul className="menu_header">
-                                    <li><b><a href="">About me</a></b>    </li>
-                                    <li><a href="">Relationship</a></li>
-                                    <li><a href="">Requirements</a></li>
-                                    <li><a href="">Users</a></li>
-                                    <li><a href="">Sign Up</a></li>
-                                </ul>
-                            </Col>
+                        <div className="header_menu">
+                           <ul className="header_menu_ul">
+                               <li><a href="">About me</a></li>
+                               <li><a href="">Relationships </a></li>
+                               <li><a href="">Requirements</a></li>
+                               <li><a href="">Users</a></li>
+                               <li><a href="">Sign Up </a></li>
+                           </ul>
+                        </div>
 
-                            <Col xs="3">
-                                <div className="text">
-                                    <p className="name">
-                                        Superstar
-                                    </p>
-                                    <p className="email">
-                                        Superstar@gmail.com
-                                    </p>
-                                </div>
+                        <div className="header_icons">
+                          <div className="text">
+                                <p className="name">
+                                    Superstar
+                                </p>
 
-                            </Col>
+                                <p className="email">
+                                    Superstar@gmail.com
+                                </p>
+                            </div>
 
 
+                            <div className="header_img">
+                                <img src={circleimg2} className="circleimg2" alt=""/>
+                                <img src={icon} alt="" className="icn"/>
+
+                            </div>
+                        </div>
 
 
-                        </Row>
-                    </Container>
+                    </div>
+                </div>
+
+                <div className="bag_photo">
+                    <div className="center_2560">
+                        <div className="bg_photo_txt">
+                            <h1 className="title">
+                                Test assignment for <br/>
+                                Frontend Developer <br/>
+                                position
+                            </h1>
+                                <p className="bg_txt">
+                                    We kindly remind you that your test assignment should <br/>
+                                    be submitted as a link to github/bitbucket repository. <br/>
+                                    Please be patient, we consider and respond to every application <br/>
+                                    that meets minimum requirements. We look forward to your <br/>
+                                    submission. Good luck!
+                                </p>
+                            <button className="btn_bg">Sign Up</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
